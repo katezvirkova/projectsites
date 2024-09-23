d77297dad8bd627dde9d2f646a2ff571d9e84032
Звіркова <katezvirkova@gmail.com>
2024-09-23 16:07:10 +0300

    changed admin.py

--- news/admin.py
+++ news/admin.py
@@ -1,15 +1,5 @@
 from django.contrib import admin
 from .models import News, Author
 
-
-@admin.register(News)
-class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_date')  #
-    search_fields = ('title', 'author__first_name', 'author__last_name')
-    list_filter = ('publication_date', 'author')
-
-
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'bio')
-    search_fields = ('first_name', 'last_name')
+admin.site.register(News)
+admin.site.register(Author)
