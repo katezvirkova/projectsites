09aed086531e48f332b060968db273a23a1f1cdb
Звіркова <katezvirkova@gmail.com>
2024-09-23 10:49:40 +0300

    created a website with the 5 latest news, as well as go to the page where all the news will be, there is also a page with authors and links to their articles, as well as the ability to edit articles and add new ones

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import News, Author
 
-# Register your models here.
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_date')  #
+    search_fields = ('title', 'author__first_name', 'author__last_name')
+    list_filter = ('publication_date', 'author')
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'bio')
+    search_fields = ('first_name', 'last_name')
--- news/views.py
+++ news/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import News, Author
 
-# Create your views here.
+def home(request):
+    latest_news = News.objects.order_by('-publication_date')[:5]
+    return render(request, 'news/home.html', {'latest_news': latest_news})
+
+def news_list(request):
+    all_news = News.objects.order_by('-publication_date')[:6]
+    return render(request, 'news/news_list.html', {'all_news': all_news})
+
+def news_detail(request, new_id):
+    news = News.objects.get(id=new_id)
+    return render(request, 'news/news_details.html', {'news': news})
+
+def authors(request):
+    all_authors = Author.objects.all()
+    return render(request, 'news/authors.html', {'all_authors': all_authors})
+
+def author_news(request, author_id):
+    author = Author.objects.get(id=author_id)
+    news_by_author = News.objects.filter(author=author)
+    return render(request, 'news/author_news.html', {'author': author, 'news_by_author': news_by_author})
